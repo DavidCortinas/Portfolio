4e82d677351c3f1a564ce21b95cf7dfd0022c780
davidcortinas <dcortinas33@gmail.com>
2021-07-08 14:57:29 -0500

    redeploy of portfolio on heroku

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
-<?php include_once("index.html, ChromePhp.php"); 
-ChromePhp::log('Hello console!');
-ChromePhp::log($_SERVER);
-ChromePhp::warn('something went wrong!');?>
+<?php include_once("index.html");?>
